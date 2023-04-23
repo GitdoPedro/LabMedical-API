5de700d54644eb8dafa44b54b285c50c7c058110
GitdoPedro <emailpedrolima@gmail.com>
2023-04-23 14:21:59 -0300

    classe pessoa

--- src/main/java/com/example/LABMedical/model/Pessoa.java
+++ src/main/java/com/example/LABMedical/model/Pessoa.java
@@ -1,4 +1,30 @@
 package com.example.LABMedical.model;
 
-public class Pessoa {
+import java.time.LocalDate;
+
+public abstract class Pessoa {
+    private Integer id;
+    private String nomeCompleto;
+    private String genero;
+    private LocalDate dataNascimento;
+    private String CPF;
+    private String RG;
+    private String estadoCivil;
+    private String telefone;
+    private String email;
+    private String Naturalidade;
+
+    public Pessoa(Integer id, String nomeCompleto, String genero, LocalDate dataNascimento, String CPF, String RG, String estadoCivil, String telefone, String email, String naturalidade) {
+        this.id = id;
+        this.nomeCompleto = nomeCompleto;
+        this.genero = genero;
+        this.dataNascimento = dataNascimento;
+        this.CPF = CPF;
+        this.RG = RG;
+        this.estadoCivil = estadoCivil;
+        this.telefone = telefone;
+        this.email = email;
+        Naturalidade = naturalidade;
+    }
+
 }
